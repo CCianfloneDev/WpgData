55faad49a209e1604d63a7c090dc2a0c105a29ef
Cole Cianflone <colecianflone123@gmail.com>
2025-02-14 15:42:40 -0600

    Upgraded table to MudDataGrid for better sorting and filtering, changed to dark mode.

--- Finance/Components/Data/WardExpense.cs
+++ Finance/Components/Data/WardExpense.cs
@@ -6,30 +6,30 @@ namespace Finance.Components.Data
     public class WardExpense
     {
         [JsonPropertyName("ward_id")]
-        public string? WardId { get; set; }
+        public  string? WardId { get; set; }
 
         [JsonPropertyName("ward_office")]
-        public string? WardOffice { get; set; }
+        public  string? WardOffice { get; set; }
 
         [JsonPropertyName("council_member")]
-        public string? CouncilMember { get; set; }
+        public  string? CouncilMember { get; set; }
 
         [JsonPropertyName("journal_date")]
-        public DateTime? JournalDate { get; set; }
+        public DateTime JournalDate { get; set; }
 
         [JsonPropertyName("vendor")]
-        public string? Vendor { get; set; }
+        public  string? Vendor { get; set; }
 
         [JsonPropertyName("expense_type")]
         public string? ExpenseType { get; set; }
 
         [JsonPropertyName("description")]
-        public string? Description { get; set; }
+        public  string? Description { get; set; }
 
         [JsonPropertyName("account")]
-        public string? Account { get; set; }
+        public  string? Account { get; set; }
 
         [JsonPropertyName("amount")]
-        public string? Amount { get; set; }
+        public  string? Amount { get; set; }
     }
 }
